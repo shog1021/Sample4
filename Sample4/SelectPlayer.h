a7c7a691643a5e59953c28d9823d8422905e0b0a
Shoji Nakamura <sho-g_no-7@kdp.biglobe.ne.jp>
2013-08-05 00:39:21 +0900

    サッカーボードの画面遷移まで。

---
+++ Sample4/SelectPlayer.h
@@ -0,0 +1,13 @@
+//
+//  SelectPlayer.h
+//  Sample4
+//
+//  Created by 中村 祥二 on 13/08/05.
+//  Copyright (c) 2013年 中村 祥二. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SelectPlayer : UIViewController
+
+@end
---
+++ Sample4/SelectPlayer.m
@@ -0,0 +1,38 @@
+//
+//  SelectPlayer.m
+//  Sample4
+//
+//  Created by 中村 祥二 on 13/08/05.
+//  Copyright (c) 2013年 中村 祥二. All rights reserved.
+//
+
+#import "SelectPlayer.h"
+
+@interface SelectPlayer ()
+
+@end
+
+@implementation SelectPlayer
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
